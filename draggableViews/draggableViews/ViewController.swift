889c3d8e280c491df5d40d0a92debb586b2f5dee
Vijay Tholpadi <vijay@thegeekprojekt.com>
2014-07-24 23:49:31 +0530

    Initial commit with project files.

---
+++ draggableViews/draggableViews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  draggableViews
+//
+//  Created by Vijay Tholpadi on 7/24/14.
+//  Copyright (c) 2014 TheGeekProjekt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
